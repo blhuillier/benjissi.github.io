3ed50298a03112a7b7d04543483775f09f2165d9
Benjamin L'Huillier <benjamin@sejong.ac.kr>
2024-11-09 16:35:10 +0900

    index.js

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const HomePage = () => (
+  <main>
+    <h1>Hello, welcome to my site!</h1>
+    <p>More content coming soon.</p>
+  </main>
+)
+
+export default HomePage
