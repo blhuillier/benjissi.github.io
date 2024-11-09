ec4e082ca0baf065ee61c94cf9813c283ade9961
Benjamin L'Huillier <lhuillier.benjamin@gmail.com>
2024-11-09 15:34:37 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,38 @@
-Copygatsby-config.js: copy code to clipboard
 module.exports = {
-  pathPrefix: "/blhuillier.github.io",
-}
+  pathPrefix: "/blhuillier.github.io", // Required for GitHub Pages
+  
+  siteMetadata: {
+    title: "Benjamin L'Huillier - Cosmologist & Astrophysicist",
+    description: "Benjamin L'Huillier's Homepage.",
+    author: "Benjamin L'Huillier",
+    siteUrl: "https://blhuillier.github.io",
+  },
+
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Benjamin L'Huillier's Website",
+        short_name: "BLHuillier",
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `standalone`,
+        icon: `src/images/favicon.png`, // Add a favicon in src/images
+      },
+    },
+    `gatsby-plugin-react-helmet`, // For SEO metadata
+    `gatsby-plugin-sitemap`, // For automatic sitemap generation
+    `gatsby-plugin-styled-components`, // Optional: for styling
+  ],
+};
