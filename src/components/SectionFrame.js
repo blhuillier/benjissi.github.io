d04126ced90e5dc73f1254fc3ace350b0bed50d3
Benjamin L'Huillier <benjamin@sejong.ac.kr>
2024-11-10 09:34:05 +0900

    deal wth no background images

--- src/components/SectionFrame.js
+++ src/components/SectionFrame.js
@@ -2,7 +2,10 @@ import React from "react"
 import "./SectionFrame.css"
 
 const SectionFrame = ({ title, dateRange, description, image }) => (
-  <div className="section-frame" style={{ backgroundImage: image ? `url(${image})` : "none" }}>
+  <div 
+    className="section-frame" 
+    style={{ backgroundImage: image ? `url(${image})` : "none" }}
+  >
     <div className="section-content">
       <h3>{title}</h3>
       <p className="section-dates">{dateRange}</p>
